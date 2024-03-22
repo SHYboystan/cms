5333cba88047359c944d7f25ba48d29879ee8b23
yong.tan <Yong.tan@bangcle.com>
2024-03-22 17:31:42 +0800

    增加步骤条

--- src/views/user/user.vue
+++ src/views/user/user.vue
@@ -9,12 +9,31 @@
       >分析页面</a-button
     >
     <a-button type="primary" @click="router.push('user')">用户页面</a-button>
+    <a-steps
+        :current="1"
+        :items="[
+      {
+        title: 'Finished',
+        description,
+      },
+      {
+        title: 'In Progress',
+        description,
+        subTitle: 'Left 00:00:08',
+      },
+      {
+        title: 'Waiting',
+        description,
+      },
+    ]"
+    ></a-steps>
   </div>
 </template>
 
 <script lang="ts" setup>
 import { useRouter } from "vue-router";
 const router = useRouter();
+const description = 'This is a description.';
 </script>
 
 <style scoped lang="scss"></style>
