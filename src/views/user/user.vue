b3d1a1cb632846b5a81ec94d356aacc03e00679d
yong.tan <Yong.tan@bangcle.com>
2024-03-29 17:31:41 +0800

    增加下划线

--- src/views/user/user.vue
+++ src/views/user/user.vue
@@ -27,6 +27,11 @@
       },
     ]"
     ></a-steps>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne merninisti licere mihi ista
+      probare, quae sunt a te dicta? Refert tamen, quo modo.
+    </p>
+    <a-divider />
   </div>
 </template>
 
